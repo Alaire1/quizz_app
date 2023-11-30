32ab3cbd8935de84c45006cda48f534ce9532f0e
Gl Tats <74324703+gltats@users.noreply.github.com>
2023-11-30 00:57:37 +0000

    footer

--- src/components/header_footer/Footer.jsx
+++ src/components/header_footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <div className="footer_copyright">
                     Made with  
                 <span style={{color : 'crimson'}}>♥</span> By 
-                <a href="http://www.github.com/Jatin-8898" target="_blank"           rel="noopener noreferrer"> Jatin Varlyani 
+                <a href="http://www.github.com/gltats" target="_blank"           rel="noopener noreferrer"> 42 Wolfsburg students
                 </a>
                 
                 </div>
