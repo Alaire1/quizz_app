e4d26c9586088812e075135b0279918609782f3a
uknowWho <66947064+mdabir1203@users.noreply.github.com>
2023-11-30 01:32:14 +0000

    added 30 sets

--- src/components/questions_data/data.js
+++ src/components/questions_data/data.js
@@ -9,7 +9,6 @@ export default [
         ],
         correct: 2
     },
-
     {
         question: 'What is the purpose of a firewall in a computer network?',
         answers: [
@@ -69,5 +68,228 @@ export default [
             'None of the Above'
         ],    
         correct: 2
+    },
+    {
+        question: "Which of the following is a common indicator of a phishing attempt?",
+        answers: [
+            "An email from a known colleague asking to verify account information",
+            "A message with multiple spelling and grammar errors",
+            "A system update notification from the IT department",
+            "A pop-up message offering a free antivirus scan"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the role of a 'packet filter' in network security?",
+        answers: [
+            "To categorize emails into folders",
+            "To examine and control network traffic based on predefined rules",
+            "To encrypt sensitive data",
+            "To track the physical location of network devices"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the significance of a 'security audit'?",
+        answers: [
+            "To boost internet speed",
+            "To assess and ensure the effectiveness of security measures",
+            "To create secure login credentials",
+            "To analyze website traffic patterns"
+        ],
+        correct: 1
+    },
+    {
+        question: "What does 'biometric authentication' involve?",
+        answers: [
+            "Using biological organisms for cybersecurity",
+            "Verifying identity through physical or behavioral characteristics",
+            "Creating unique passwords for each login",
+            "Implementing two-factor authentication"
+        ],
+        correct: 1
+    },
+    {
+        question: "How does a 'proxy server' contribute to cybersecurity?",
+        answers: [
+            "By blocking all incoming traffic",
+            "By acting as an intermediary between a user's device and the internet",
+            "By encrypting all data transmitted over a network",
+            "By providing antivirus protection"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the purpose of 'security patches'?",
+        answers: [
+            "To fix bugs in software",
+            "To create secure passwords",
+            "To enhance internet connectivity",
+            "To initiate a system backup"
+        ],
+        correct: 0
+    },
+    {
+        question: "How does a 'sandbox' contribute to cybersecurity?",
+        answers: [
+            "By creating a secure play area for developers",
+            "By blocking access to certain websites",
+            "By detecting and removing malware",
+            "By simulating a controlled environment for testing untrusted programs"
+        ],
+        correct: 3
+    },
+    {
+        question: "What is the purpose of 'intrusion detection systems (IDS)'?",
+        answers: [
+            "To create secure email accounts",
+            "To identify and respond to suspicious activities or security policy violations",
+            "To improve internet speed",
+            "To encrypt data during transmission"
+        ],
+        correct: 1
+    },
+    {
+        question: "How does a 'token' enhance authentication?",
+        answers: [
+            "By generating one-time passwords",
+            "By blocking unauthorized access",
+            "By providing unlimited access to all users",
+            "By encrypting stored data"
+        ],
+        correct: 0
+    },
+    {
+        question: "What is the role of 'cryptography' in information security?",
+        answers: [
+            "To develop secure software applications",
+            "To analyze internet traffic patterns",
+            "To store passwords in a secure database",
+            "To protect information through encryption and decryption"
+        ],
+        correct: 3
+    },
+    {
+        question: "What does 'vulnerability assessment' involve?",
+        answers: [
+            "Identifying weaknesses in a system and assessing potential threats",
+            "Creating secure user interfaces",
+            "Enhancing network connectivity",
+            "Implementing two-factor authentication"
+        ],
+        correct: 0
+    },
+    {
+        question: "How does 'endpoint security' contribute to overall cybersecurity?",
+        answers: [
+            "By securing physical entry points to a building",
+            "By protecting network devices such as computers and mobile devices",
+            "By improving internet speed",
+            "By encrypting data during transmission"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is 'pharming' in the context of cybersecurity?",
+        answers: [
+            "Cultivating cybersecurity professionals",
+            "Redirecting users to fraudulent websites",
+            "A type of firewall configuration",
+            "Encrypting sensitive data"
+        ],
+        correct: 1
+    },
+    {
+        question: "How does a 'security token' enhance user authentication?",
+        answers: [
+            "By granting unlimited access to all users",
+            "By generating one-time passwords",
+            "By blocking unauthorized access",
+            "By encrypting stored data"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the primary goal of 'security awareness training'?",
+        answers: [
+            "To enhance internet speed",
+            "To educate users about potential cybersecurity threats and best practices",
+            "To create complex passwords",
+            "To improve the performance of security software"
+        ],
+        correct: 1
+    },
+    {
+        question: "How does 'data masking' contribute to data security?",
+        answers: [
+            "By completely blocking access to certain websites",
+            "By encrypting sensitive data during transmission",
+            "By simulating a controlled environment for testing untrusted programs",
+            "By replacing sensitive information with fake or pseudonymous data"
+        ],
+        correct: 3
+    },
+    {
+        question: "What is the significance of 'security incident response'?",
+        answers: [
+            "To boost internet speed",
+            "To analyze website traffic patterns",
+            "To identify and respond to cybersecurity incidents",
+            "To create secure email accounts"
+        ],
+        correct: 2
+    },
+    {
+        question: "How does 'access control' contribute to information security?",
+        answers: [
+            "By securing physical entry points to a building",
+            "By regulating and restricting access to computer systems and data",
+            "By providing antivirus protection",
+            "By creating secure user interfaces"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the purpose of a 'security policy'?",
+        answers: [
+            "To enhance internet speed",
+            "To establish guidelines and practices for ensuring information security",
+            "To analyze internet traffic patterns",
+            "To fix bugs in software"
+        ],
+        correct: 1
+    },
+    {
+        question: "How does 'penetration testing' enhance cybersecurity measures?",
+        answers: [
+            "By simulating real-world cyber attacks to identify vulnerabilities",
+            "By encrypting sensitive data during transmission",
+            "By improving internet speed",
+            "By providing unlimited access to all users"
+        ],
+        correct: 0
+    },
+    {
+        question: "What does 'security through obscurity' entail?",
+        answers: [
+            "Blocking access to certain websites",
+            "Encrypting stored data with complex algorithms",
+            "Relying on the secrecy of system design to provide security",
+            "A type of intrusion detection system"
+        ],
+        correct: 2
+    },
+    {
+        question: "How does 'application layer firewall' contribute to network security?",
+        answers: [
+            "By enhancing internet speed",
+            "By regulating traffic based on application-specific rules",
+            "By blocking access to certain websites",
+            "By encrypting sensitive data during transmission"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the purpose of 'security tokens
     }
 ]
