7f607da83e6e5f4eaa5be3cf6e57e36aa8de56ae
Anita <89337095+Alaire1@users.noreply.github.com>
2023-11-30 00:47:43 +0000

    data base added

--- src/components/questions_data/data.js
+++ src/components/questions_data/data.js
@@ -1,73 +1,73 @@
 export default [
     {
-        question: 'We can go for keys when there is possibility that our user could change the data?',
+        question: 'hat does the term "Phishing" refer to in cybersecurity?',
         answers: [
-            'Keys',
-            'Ref',
-            'Both',
+            'A method of encrypting data',
+            'A social engineering attack to trick individuals into revealing sensitive information',
+            'A type of firewall',
             'None of above'
         ],
-        correct: 1
+        correct: 2
     },
 
     {
-        question: 'JSX is typesafe?',
+        question: 'What is the purpose of a firewall in a computer network?',
         answers: [
-            'True',
-            'False',
-            'None of the Above',
+            'To prevent physical damage to the computer',
+            'To block unauthorized access and control the flow of data',
+            'To boost internet speed',
             'Cannot Be Determined'
         ],
-        correct: 1
+        correct: 2
     },    
     {
-        question: 'React merges the object you provide into the current state using which of the following',
+        question: 'What is the primary function of antivirus software?',
         answers: [
-            'setState()',
-            'State()', 
-            'getState()', 
+            'To create strong passwords',
+            'To detect and remove malicious software', 
+            'To encrypt files and folders', 
             'None of the Above'
         ],    
-        correct: 1
+        correct: 2
     },    
     {
-        question: 'Arbitrary inputs of components are called?',
+        question: 'What is a DDoS attack?',
         answers: [
-            'Keys',
-            'Props', 
-            'Elements', 
+            'A type of malware',
+            'A hacking technique to gain unauthorized access', 
+            'An attempt to make a website or online service unavailable by overwhelming it with traffic from multiple sources', 
             'Ref'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'What does VPN stand for?',
+        answers: [
+            'Virtual Personal Network',
+            'Very Private Network', 
+            'Virtual Private Network', 
+            'Wrapping'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'Which ohich of the following is an example of a strong password?',
+        answers: [
+            '123456',
+            'P@ssw0rd', 
+            'Password', 
+            'None of the Above'
         ],    
         correct: 2
     },
     {
-        question: '_____ can be done while more than one element needs to be returned from a component?',
+        question: 'What is two-factor authentication (2FA)?',
         answers: [
-            'Abstraction',
-            'Packing', 
-            'Insulation', 
-            'Wrapping'
-        ],    
-        correct: 4
-    },
-    {
-        question: 'Which of the following needs to be updated to achieve dynamic UI updates?',
-        answers: [
-            'State',
-            'Props', 
-            'Components', 
+            'Using two different types of antivirus software',
+            'A security process in which the user provides two different authentication factors to verify their identity', 
+            'Connecting to two different Wi-Fi networks simultaneously', 
             'None of the Above'
         ],    
-        correct: 1
-    },
-    {
-        question: 'Lifecycle methods are mainly used _____',
-        answers: [
-            'To keep track of event history',
-            'To enhance components', 
-            'Free up resources', 
-            'None of the Above'
-        ],    
-        correct: 3
+        correct: 2
     }
 ]
